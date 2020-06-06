e23391ecdd37987448919a3043a0ed3998efa770
Vinicius <vinigc19@gmail.com>
2020-06-06 20:08:04 -0300

    web & mobile

---
+++ web/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface HeaderProps {
+    title: string;
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
+    return (
+        <header>
+        </header>
+    )
+}
+
+export default Header;
